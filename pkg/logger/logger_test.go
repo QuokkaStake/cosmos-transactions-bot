af9f1af5bcab4c391d39abc7b285dab9e45d6d22
Sergey <freak12techno@gmail.com>
2024-02-25 04:50:03 +0300

    chore: fix linting

--- pkg/logger/logger_test.go
+++ pkg/logger/logger_test.go
@@ -1,10 +1,11 @@
 package logger_test
 
 import (
-	"github.com/stretchr/testify/require"
 	configPkg "main/pkg/config"
 	loggerPkg "main/pkg/logger"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
 func TestGetDefaultLogger(t *testing.T) {
