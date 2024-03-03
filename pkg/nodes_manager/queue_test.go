0747999cefac0e734284bd4738e77f309c9f911a
Sergey <freak12techno@gmail.com>
2024-03-03 07:30:30 +0300

    chore: test NodesManager Queue

---
+++ pkg/nodes_manager/queue_test.go
@@ -0,0 +1,25 @@
+package nodes_manager_test
+
+import (
+	"github.com/stretchr/testify/require"
+	configTypes "main/pkg/config/types"
+	nodesManagerPkg "main/pkg/nodes_manager"
+	"main/pkg/types"
+	"testing"
+)
+
+func TestQueueAdd(t *testing.T) {
+	t.Parallel()
+
+	queue := nodesManagerPkg.NewReportQueue(1)
+	report1 := types.Report{Reportable: &types.Tx{Hash: configTypes.Link{Value: "123"}}}
+	report2 := types.Report{Reportable: &types.Tx{Hash: configTypes.Link{Value: "456"}}}
+
+	queue.Add(report1)
+	require.True(t, queue.Has(report1))
+	require.False(t, queue.Has(report2))
+
+	queue.Add(report2)
+	require.True(t, queue.Has(report2))
+	require.False(t, queue.Has(report1))
+}
