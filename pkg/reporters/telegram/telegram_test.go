e290cc0e3d3754977eb426ecf3b9fd9b2c629e8a
Sergey <freak12techno@gmail.com>
2024-12-27 05:08:23 +0300

    chore: added internal error test

--- pkg/reporters/telegram/telegram_test.go
+++ pkg/reporters/telegram/telegram_test.go
@@ -3,11 +3,18 @@ package telegram
 import (
 	"errors"
 	"main/assets"
+	"main/pkg/alias_manager"
 	configPkg "main/pkg/config"
 	configTypes "main/pkg/config/types"
+	"main/pkg/data_fetcher"
+	"main/pkg/fs"
 	loggerPkg "main/pkg/logger"
+	"main/pkg/metrics"
+	"main/pkg/types"
 	"testing"
 	"time"
+
+	tele "gopkg.in/telebot.v3"
 
 	"github.com/jarcoal/httpmock"
 	"github.com/stretchr/testify/require"
@@ -142,4 +149,67 @@ func TestTelegramReporterStartsDisabled(t *testing.T) {
 	)
 
 	reporter.Start()
+}
+
+//nolint:paralleltest // disabled
+func TestHandlerInternalError(t *testing.T) {
+	httpmock.Activate()
+	defer httpmock.DeactivateAndReset()
+
+	httpmock.RegisterResponder(
+		"POST",
+		"https://api.telegram.org/botxxx:yyy/getMe",
+		httpmock.NewBytesResponder(200, assets.GetBytesOrPanic("telegram-bot-ok.json")))
+
+	httpmock.RegisterMatcherResponder(
+		"POST",
+		"https://api.telegram.org/botxxx:yyy/sendMessage",
+		types.TelegramResponseHasText("Internal error!"),
+		httpmock.NewBytesResponder(200, assets.GetBytesOrPanic("telegram-send-message-ok.json")),
+	)
+
+	logger := loggerPkg.GetDefaultLogger()
+	config := &configPkg.AppConfig{}
+	aliasManager := alias_manager.NewAliasManager(logger, config, &fs.MockFs{})
+	metricsManager := metrics.NewManager(logger, configPkg.MetricsConfig{})
+
+	reporter := NewReporter(
+		&configTypes.Reporter{
+			Name:           "reporter",
+			Type:           "telegram",
+			TelegramConfig: &configTypes.TelegramConfig{Token: "xxx:yyy", Chat: 123, Admins: []int64{1}},
+		},
+		config,
+		logger,
+		nil,
+		aliasManager,
+		metricsManager,
+		data_fetcher.NewDataFetcher(logger, config, aliasManager, metricsManager),
+		"1.2.3",
+	)
+
+	err := reporter.Init()
+	require.NoError(t, err)
+
+	context := reporter.TelegramBot.NewContext(tele.Update{
+		ID: 1,
+		Message: &tele.Message{
+			Sender: &tele.User{Username: "testuser"},
+			Text:   "/command",
+			Chat:   &tele.Chat{ID: 2},
+		},
+	})
+
+	command := Command{
+		Name:    "command",
+		MinArgs: 0,
+		Usage:   "/command",
+		Query:   "command",
+		Execute: func(c tele.Context) (string, error) {
+			return "", errors.New("custom error")
+		},
+	}
+
+	err = reporter.Handler(command)(context)
+	require.NoError(t, err)
 }
